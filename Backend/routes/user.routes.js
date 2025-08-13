a58581395af299cdb5f72111c0613e05d1d3908d
dinesh-2047 <dinesh07bhardwaj@gmail.com>
2025-08-14 01:58:26 +0530

    user backend added

--- Backend/routes/user.routes.js
+++ Backend/routes/user.routes.js
@@ -1,1 +1,21 @@
-// For user routes 
+import express from "express";
+import {
+  registerUser,
+  loginUser,
+  logoutUser,
+  refreshAccessToken,
+  changeCurrentPassword,
+  getCurrentUser
+} from "../controllers/user.controller.js";
+import { verifyJWT } from "../middlewares/auth.middleware.js";
+
+const router = express.Router();
+
+router.post("/register", registerUser);
+router.post("/login", loginUser);
+router.post("/logout", verifyJWT, logoutUser);
+router.post("/refresh-token", refreshAccessToken);
+router.post("/change-password", verifyJWT, changeCurrentPassword);
+router.get("/current-user", verifyJWT, getCurrentUser);
+
+export default router;
